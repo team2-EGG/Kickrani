4925ef77e0c0a123b5827fae115f01aed6bb8773
SJY <jun000405@naver.com>
2023-12-05 12:52:27 +0900

    fix : 고라니 7로 업데이트 (내부 스크립트 삭제 및 에셋 의존성 조정)

---
+++ KickRani by EGG team 2/Assets/gorani_file/PlayerScripts/PlayerController.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector3 ShieldSize = new Vector3(2.5f, 2.5f, 2.5f);
+    public bool isShield = false;
+    public bool isHelmet = false;
+    public bool isStar = false;
+    Transform Shield;
+    Transform Helmet;
+    Material meshMaterial;
+    Coroutine colorChangeCoroutine;
+
+    void Start()
+    {
+        
+        if (Shield == null)
+            Debug.LogError("Shield 오브젝트를 찾을 수 없습니다.");
+
+        if (Helmet == null)
+            Debug.LogError("Helmet 오브젝트를 찾을 수 없습니다.");
+
+        Transform goraTransform = transform.Find("gora");
+        if (goraTransform == null)
+        {
+            Debug.LogError("gora 오브젝트를 찾을 수 없습니다.");
+            return;
+        }
+
+        Shield = goraTransform.Find("Shield");
+        Helmet = goraTransform.Find("MESH_Hat_Skater");
+        Transform meshTransform = goraTransform.Find("Mesh");
+        if (meshTransform == null)
+        {
+            Debug.LogError("Mesh 오브젝트를 찾을 수 없습니다.");
+            return;
+        }
+
+        Renderer meshRenderer = meshTransform.GetComponent<Renderer>();
+        if (meshRenderer != null)
+            meshMaterial = meshRenderer.material;
+        else
+            Debug.LogError("Mesh 오브젝트에 Renderer 컴포넌트가 없습니다.");
+    }
+
+    void Update()
+    {
+        if (Shield != null)
+        {
+            Shield.localScale = isShield ? ShieldSize : Vector3.zero;
+            Debug.Log(isShield ? "실드 활성화됨" : "실드 비활성화됨");
+        }
+
+        if (Helmet != null)
+        {
+            Helmet.localScale = isHelmet ? Vector3.one : Vector3.zero;
+            Debug.Log(isHelmet ? "헬멧 활성화됨" : "헬멧 비활성화됨");
+        }
+
+        if (isStar && meshMaterial != null && colorChangeCoroutine == null)
+        {
+            colorChangeCoroutine = StartCoroutine(ChangeColor());
+        }
+        else if (!isStar && colorChangeCoroutine != null)
+        {
+            StopCoroutine(colorChangeCoroutine);
+            colorChangeCoroutine = null;
+            meshMaterial.color = Color.white; // 색상을 원래대로 설정
+        }
+    }
+
+    IEnumerator ChangeColor()
+    {
+        Color[] baseColors = new Color[] { Color.red, Color.white, Color.yellow, Color.green, Color.blue, Color.cyan, Color.magenta};
+        Color[] colors = new Color[baseColors.Length];
+        for (int i = 0; i < baseColors.Length; i++)
+        {
+            colors[i] = ReduceBrightness(baseColors[i], 0.4f); // 명도를 ?% 줄인 색상으로 설정
+        }
+
+        int index = 0;
+
+        while (true)
+        {
+            meshMaterial.color = colors[index];
+            index = (index + 1) % colors.Length;
+            meshMaterial.SetFloat("_Emission", 0.5f);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    // 명도를 줄이는 함수
+    Color ReduceBrightness(Color color, float brightnessFactor)
+    {
+        color.r *= brightnessFactor;
+        color.g *= brightnessFactor;
+        color.b *= brightnessFactor;
+        return color;
+    }
+}
---
+++ KickRani by EGG team 2/Assets/gorani_file/PlayerScripts/Player_movecontroller.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movecontroller : MonoBehaviour
+{
+    // player move
+    public float moveSpeed = 100.0f; // 이동 속도
+    private Rigidbody rb;
+    // player turn
+    float turnAngle = 90f;
+    public float turnSpeed = 500f;
+    bool turnActive = false;
+    int direction = 0;
+    // player death
+    private float rotationAmount = 90f; // 회전할 각도
+    private float duration = 0.6f; // 회전하는데 걸리는 시간 (초)
+    private float rotationSpeed; // 초당 회전 속도
+    private float currentRotation = 0f; // 현재 회전된 각도
+    private bool isRotating = false; // 회전 중인지 확인
+    // player go forward
+    public float speed = 10f; // 전진 속도
+    // player jump
+    public float jumpForce = 10f;
+    public Transform childObject;
+    private bool isGrounded;
+    private Vector3 originalChildPosition;
+    private Quaternion originalChildRotation;
+    // 공중에 떠 있을 때 적용할 위치와 회전값
+    private Vector3 tempPosition = new Vector3(-0.3f, 0.2f, 0f);
+    private Quaternion tempRotation = Quaternion.Euler(25f, 90f, 0f);
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rotationSpeed = rotationAmount / duration; // 초당 회전 속도 계산
+        originalChildPosition = childObject.localPosition;
+        originalChildRotation = childObject.localRotation;
+
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal"); // 좌우 이동 입력
+
+        // 좌우 입력에 따라 위나 아래로 이동
+        Vector3 movementDirection = (horizontalInput > 0) ? -transform.forward : transform.forward;
+        Vector3 movement = movementDirection * Mathf.Abs(horizontalInput) * moveSpeed * Time.deltaTime;
+
+        // 오브젝트의 위치 업데이트
+        rb.MovePosition(rb.position + movement);
+
+        // 플레이어 턴 관련
+        if (Input.GetKeyDown(KeyCode.Z) && turnActive == false)
+        {
+            turnActive = true;
+            direction = -1;
+        }
+        else if (Input.GetKeyDown(KeyCode.X) && turnActive == false)
+        {
+            turnActive = true;
+            direction = 1;
+        }
+
+        if (turnActive == true)
+        {
+
+
+            Vector3 turn = new Vector3(0, turnSpeed, 0);
+            transform.Rotate(turn * direction * Time.deltaTime);
+            turnAngle -= turnSpeed * Time.deltaTime;
+
+            if (turnAngle <= 0)
+            {
+                turnActive = false;
+                turnAngle = 90.0f;
+                Vector3 currentRotation = transform.eulerAngles;
+                currentRotation.y = Mathf.Round(currentRotation.y / 90f) * 90f;
+                transform.eulerAngles = currentRotation;
+
+            }
+        }
+
+        // 플레이어 사망 관련
+        // 'R' 키를 눌러 회전 시작
+        if (Input.GetKeyDown(KeyCode.R) && !isRotating)
+        {
+            isRotating = true;
+        }
+
+        // 회전 로직
+        if (isRotating)
+        {
+            float step = rotationSpeed * Time.deltaTime;
+            transform.Rotate(-step, 0, 0);
+            currentRotation += step;
+
+            if (currentRotation >= rotationAmount)
+            {
+                isRotating = false;
+                currentRotation = 0f;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q) && !isRotating) // Q키 누르면 다시 원 위치
+        {
+            this.transform.rotation = (Quaternion.Euler(0, -90, 0));
+        }
+
+        // 플레이어 전진 관련
+        // 전진 방향을 계산 (오브젝트의 앞쪽 방향)
+        Vector3 right = transform.right;
+
+        // 리지드바디를 사용하여 오브젝트를 전진시킴
+        rb.MovePosition(rb.position + right * speed * Time.fixedDeltaTime);
+
+        // 플레이어 점프 관련
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+            isGrounded = false;
+
+            // 위치 및 회전 변경
+            childObject.localPosition = tempPosition;
+            childObject.localRotation = tempRotation;
+        }
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+
+            childObject.localPosition = originalChildPosition;
+            childObject.localRotation = originalChildRotation;
+        }
+    }
+
+}
