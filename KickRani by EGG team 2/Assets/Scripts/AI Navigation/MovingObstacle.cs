0b8451c4edaf975651c2c7e59fede72a81dac37c
Shin714 <hshin714@gmail.com>
2023-12-15 17:45:06 +0900

    dd

--- KickRani by EGG team 2/Assets/Scripts/AI Navigation/MovingObstacle.cs
+++ KickRani by EGG team 2/Assets/Scripts/AI Navigation/MovingObstacle.cs
@@ -7,7 +7,8 @@ using Unity.AI.Navigation;
 public class MovingObstacle : MonoBehaviour
 {
     NavMeshSurface navMeshSurface;
-
+    public Transform parentObject; // 부모 오브젝트를 할당할 변수
+    public float teleportRange = 26f; // 텔레포트할 범위
     NavMeshAgent agent;
     public bool A = false;
     public GameObject T;
@@ -19,13 +20,13 @@ public class MovingObstacle : MonoBehaviour
 
 
     [SerializeField]
-    Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
-        target = T.transform;
         baker = B.GetComponent<Baker>();
         agent = GetComponent<NavMeshAgent>();
+        parentObject = T.transform.parent;
         navMeshSurface = this.gameObject.GetComponent<NavMeshSurface>();
         initialRelativePosition = transform.localPosition;
     }
@@ -38,18 +39,30 @@ public class MovingObstacle : MonoBehaviour
 
     void OnEnable()
     {
-        //agent.enabled = true;
+
         baker.BakeRoad();
-        agent.SetDestination(target.position);
+        TeleportWithinRange();
+        agent.SetDestination(T.transform.position);
 
         RestoreToInitialState();
     }
 
-    private void OnDisable()
+    void TeleportWithinRange()
     {
-        //agent.enabled = false;
+        if (parentObject != null)
+        {
+            // 부모의 위치를 기준으로 랜덤한 위치로 텔레포트
+            Vector3 randomPosition = parentObject.position + new Vector3(26, 0, -4) + Random.insideUnitSphere * teleportRange;
+
+            // Y 좌표를 부모와 동일하게 설정하여 수직 이동을 방지
+            randomPosition.y = parentObject.position.y;
+
+            // 현재 위치를 랜덤한 위치로 변경
+            T.transform.position = randomPosition;
+        }
+        else
+        {
+            Debug.LogError("부모 오브젝트가 지정되지 않았습니다.");
+        }
     }
-
-    // Update is called once per frame
-
 }
